7f2fcae275eb6a20b902d5298f978e2eae5a19ea
DESKTOP-FMII11R\zi-ku <program922@hotmail.com>
2021-09-16 01:58:42 +0900

    SHOWボタンにし，それを押したときにSAMPLEの文字を第二画面に出力するようにプログラムした． WndProc関数は，呼び出しがあるたび一番上から走査してプログラムを実行することに注意． 変数などをWndProc関数の最初に書くと毎回呼び出され，変数を書き換えるようなプログラムを書いても，初期値を設定している場合などは毎回その値に戻ってしまう．

--- LCOS_app/LCOS_app.cpp
+++ LCOS_app/LCOS_app.cpp
@@ -5,7 +5,7 @@
 #include "LCOS_app.h"
 
 #define ID_SEND 100
-#define ID_STOP 101
+#define ID_SHOW 101
 
 #define MAX_LOADSTRING 100
 #define WINDOW_WIDE 400
@@ -22,6 +22,22 @@ BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
+// 二つ目のウィンドウについて
+LPCWSTR szWindowClass2 = L"BitMap Window";
+ATOM BitMapClass(HINSTANCE hInstance);
+LRESULT CALLBACK WndProc2(HWND, UINT, WPARAM, LPARAM);
+
+//モニター情報取得関連
+HMONITOR hMonitor;
+MONITORINFOEX MonitorInfoEx;
+POINT pt = { 1921, 0 };
+
+//BITMAPに必要な変数
+HDC hdc_men = 0;
+HDC hdc_men1 = 0;
+int w = 0, h = 0;
+int number = 0;
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPWSTR    lpCmdLine,
@@ -36,6 +52,12 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
     LoadStringW(hInstance, IDC_LCOSAPP, szWindowClass, MAX_LOADSTRING);
     MyRegisterClass(hInstance);
+    BitMapClass(hInstance);
+    
+    //モニター情報の取得関連
+    hMonitor = MonitorFromPoint(pt, MONITOR_DEFAULTTONEAREST);
+    MonitorInfoEx.cbSize = sizeof(MonitorInfoEx);
+    GetMonitorInfo(hMonitor, &MonitorInfoEx);//MonitorInfoEx内に情報が格納される
 
     // アプリケーション初期化の実行:
     if (!InitInstance (hInstance, nCmdShow))
@@ -88,6 +110,27 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     return RegisterClassExW(&wcex);
 }
 
+//二つ目のウィンドクラスの登録
+ATOM BitMapClass(HINSTANCE hInstance) {
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc2;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_LCOSAPP));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDI_LCOSAPP);
+    wcex.lpszClassName = szWindowClass2;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
 //
 //   関数: InitInstance(HINSTANCE, int)
 //
@@ -102,8 +145,21 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // グローバル変数にインスタンス ハンドルを格納する
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+   HWND hWnd = CreateWindowW(szWindowClass, TEXT("操作メニュー"), WS_OVERLAPPEDWINDOW,
       CW_USEDEFAULT, 0, WINDOW_WIDE, WINDOW_HIGH, nullptr, nullptr, hInstance, nullptr);
+
+
+   HWND hWnd2 = CreateWindowW(szWindowClass2, //クラス名
+       TEXT("BmpWindow"),			        //ウィンドウ名
+       WS_POPUPWINDOW,					//ウィンドウスタイル
+       MonitorInfoEx.rcMonitor.left,	    //X位置
+       MonitorInfoEx.rcMonitor.top,			//Y位置
+       MonitorInfoEx.rcMonitor.right- MonitorInfoEx.rcMonitor.left,	    //ウィンドウ幅
+       MonitorInfoEx.rcMonitor.bottom- MonitorInfoEx.rcMonitor.top,	    //ウィンドウ高さ
+       nullptr,								//親ウィンドウのハンドル，親を作るときはnullptr
+       nullptr,								//メニューハンドル，クラスメニューを使うときはnullptr
+       hInstance,							//インスタンスハンドル
+       nullptr);							//ウィンドウ作成データ
 
    if (!hWnd)
    {
@@ -112,6 +168,9 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 
    ShowWindow(hWnd, nCmdShow);
    UpdateWindow(hWnd);
+
+   ShowWindow(hWnd2, nCmdShow);
+   UpdateWindow(hWnd2);
 
    return TRUE;
 }
@@ -151,6 +210,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                 DestroyWindow(hWnd);
                 break;
             case ID_SEND:
+                //SENDボタンを押したときの動作
                 hWnd = FindWindow(NULL, TEXT("Chamonix"));
                 if (hWnd != 0) {
                     MessageBox(hWnd, TEXT("Chamonixが開かれています"), TEXT("成功"), MB_OK);
@@ -172,7 +232,14 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                     MessageBox(hWnd, TEXT("Chamonixが開かれていません"), TEXT("エラー"), MB_OK);
                 }
                 break;
-            case ID_STOP:
+            case ID_SHOW:
+                //SHOWボタンを押したときの動作
+                hWnd = FindWindow(NULL, TEXT("BmpWindow"));
+                if (hWnd != 0) {
+                    //MessageBox(hWnd, TEXT("BmpWindowが開かれています"), TEXT("成功"), MB_OK);
+                    SendMessage(hWnd, IDB_SHOW, NULL, NULL);
+                }
+
                 break;
             default:
                 return DefWindowProc(hWnd, message, wParam, lParam);
@@ -181,17 +248,77 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         break;
     case WM_CREATE:
         hSend = CreateWindow(TEXT("BUTTON"), TEXT("SEND"), WS_CHILD | WS_VISIBLE, 10, 10, 100, 30, hWnd, (HMENU)ID_SEND, hInst, NULL);
-        hStop = CreateWindow(TEXT("BUTTON"), TEXT("STOP"), WS_CHILD | WS_VISIBLE, 10, 50, 100, 30, hWnd, (HMENU)ID_STOP, hInst, NULL);
+        hStop = CreateWindow(TEXT("BUTTON"), TEXT("SHOW"), WS_CHILD | WS_VISIBLE, 10, 50, 100, 30, hWnd, (HMENU)ID_SHOW, hInst, NULL);
         break;
     case WM_PAINT:
         {
-
             hdc = BeginPaint(hWnd, &ps);
             // TODO: HDC を使用する描画コードをここに追加してください...
             EndPaint(hWnd, &ps);
         }
         break;
     case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+//二つ目のウィンドウのCallBack
+LRESULT CALLBACK WndProc2(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    int wmId, wmEvent;
+    PAINTSTRUCT ps;
+    HDC hdc;
+
+    HBITMAP hBmp = 0;
+    BITMAP bmp_info;
+
+    switch (message) {
+    case WM_COMMAND:
+        wmId = LOWORD(wParam);
+        wmEvent = HIWORD(wParam);
+        // 選択されたメニューの解析:
+        switch (wmId) {
+        case IDM_ABOUT:
+            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+            break;
+        case IDM_EXIT:
+            DestroyWindow(hWnd);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+        break;
+    case WM_CREATE:
+        //デバイスコンテキストhdc_menにビットマップリソース「SAMPLE」を読み込む
+        hdc = GetDC(hWnd);
+        hBmp = LoadBitmap(hInst, TEXT("SAMPLE"));
+        hdc_men1 = CreateCompatibleDC(hdc);
+        SelectObject(hdc_men1, hBmp);
+        GetObject(hBmp, (int)sizeof(BITMAP), &bmp_info);//bitmap画像の取得
+        w = bmp_info.bmWidth;
+        h = bmp_info.bmHeight;
+        ReleaseDC(hWnd, hdc);
+        break;
+    case IDB_SHOW:
+        number = 1;
+        InvalidateRect(hWnd, NULL, TRUE);
+        break;
+
+    case WM_PAINT:
+        hdc = BeginPaint(hWnd, &ps);
+        // TODO: 描画コードをここに追加してください...
+        if (number == 1) {
+            hdc_men = hdc_men1;
+        }
+        BitBlt(hdc, (MonitorInfoEx.rcMonitor.right - MonitorInfoEx.rcMonitor.left) / 2 - w / 2, (MonitorInfoEx.rcMonitor.bottom - MonitorInfoEx.rcMonitor.top) / 2 - h / 2, w, h, hdc_men, 0, 0, SRCCOPY);
+
+        EndPaint(hWnd, &ps);
+        break;
+    case WM_DESTROY:
+        DeleteObject(hBmp);
         PostQuitMessage(0);
         break;
     default:
